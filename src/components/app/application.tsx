1d089358342bb8b61db9abcd668b9af589b0de79
Alexander Toftesund <alexandertoftesund@gmail.com>
2026-03-09 15:52:57 +0100

    Added Kommuner-S.geojson

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import { useGeographic } from "ol/proj.js";
+import TileLayer from "ol/layer/Tile.js";
+import { OSM } from "ol/source.js";
+
+import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
+
+useGeographic();
+
+const kommuneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/kommuner.geojson",
+    format: new GeoJSON(),
+  }),
+});
+
+const grunnskoleLayer = new VectorLayer();
+const map = new Map({
+  view: new View({ center: [10.7, 59.9], zoom: 8 }),
+  layers: [new TileLayer({ source: new OSM() }), kommuneLayer, grunnskoleLayer],
+});
+
+export function Application() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => map.setTarget(mapRef.current!), []);
+  return <div ref={mapRef}></div>;
+}
+
+/*
 import React, { useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
@@ -19,4 +52,4 @@ export function Application() {
   }, []);
 
   return <div ref={mapRef}></div>;
-}
+}*/
